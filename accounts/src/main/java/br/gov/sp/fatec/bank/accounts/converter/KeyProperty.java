235ff5cc31e5268310b225b341f4fa936b982658
aluno <aluno@fatec.aluno>
2018-11-29 20:32:38 -0200

    Converters para criptografia dos campos das entidades

---
+++ accounts/src/main/java/br/gov/sp/fatec/bank/accounts/converter/KeyProperty.java
@@ -0,0 +1,16 @@
+package br.gov.sp.fatec.bank.accounts.converter;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KeyProperty {
+
+    public static String DATABASE_ENCRYPTION_KEY;
+
+    @Value("${example.database.encryption.key}")
+    public void setDatabase(String databaseEncryptionKey) {
+        DATABASE_ENCRYPTION_KEY = databaseEncryptionKey;
+    }
+
+}
